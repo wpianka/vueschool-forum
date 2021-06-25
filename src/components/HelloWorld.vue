e7baa6e767f15e9aafad9172929421378bc5eeb3
Wojtek Pianka <wojtek.pianka@grupa26.pl>
2021-06-25 15:56:46 +0200

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,12 +31,15 @@
 </template>
 
 <script>
+
+
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/components/PageHome.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-for="thread in threads" :key="thread.id" >
+    <h2>{{thread.title}}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{userById(postById(postId).userId).name}}</p>
+      <p>{{postById(postId).text}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceDate from '@/data.json';
+
+export default {
+  name: 'PageHome',
+  data() {
+    return {
+      threads: sourceDate.threads,
+      posts: sourceDate.posts,
+      users: sourceDate.users,
+    };
+  },
+  methods: {
+    postById(postId) {
+      return this.posts.find((p) => p.id === postId);
+    },
+    userById(userId) {
+      return this.users.find((u) => u.id === userId);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
+W
